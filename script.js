34930940912bfff9feece06d4fdaeffc4daad245
Pankaj Mokashi <127533657+pankajmokashi@users.noreply.github.com>
2023-04-25 18:39:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const p = document.getElementById("status")
 const btn = document.getElementById("enterBtn")
 const body = document.querySelector("body")
 function changeText(){
+	p.remove()
     const h3 = document.createElement("h3")
     h3.innerText = "Entered Metaverse"
     body.replaceChild(h3, body.children[0])
