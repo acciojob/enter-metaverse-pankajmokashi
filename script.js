684fed5360f5c5294c7a9251ccb35044194ec7bb
Pankaj Mokashi <127533657+pankajmokashi@users.noreply.github.com>
2023-04-25 18:57:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,11 +3,9 @@ const p = document.getElementById("status")
 const btn = document.getElementById("enterBtn")
 const body = document.querySelector("body")
 function changeText(){
-	p.remove()
     const h1 = document.createElement("h1")
 	h1.id = "status"
     h1.innerText = "Entered Metaverse"
-    body.replaceChild(h1, body.children[0])
-	body.childern[0] = h1
+    body.replaceChild(h1, body.children[1])
 }
 btn.addEventListener("click", changeText)
