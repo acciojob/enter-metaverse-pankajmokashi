070c0942cf8eeee6e52dc3dd10067cac59e01f15
Pankaj Mokashi <127533657+pankajmokashi@users.noreply.github.com>
2023-04-25 18:39:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+const p = document.getElementById("status")
+const btn = document.getElementById("enterBtn")
+const body = document.querySelector("body")
+function changeText(){
+    const h3 = document.createElement("h3")
+    h3.innerText = "Entered Metaverse"
+    body.replaceChild(h3, body.children[0])
+}
+btn.addEventListener("click", changeText)
